e107c7a787f20a997b624836490af5d1ebcb39a7
ottavi <155098365+lolsono@users.noreply.github.com>
2026-05-18 21:20:18 +0200

    ajout de la home page fix

--- src/views/Main.php
+++ src/views/Main.php
@@ -14,6 +14,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?></title>
     <link rel="stylesheet" href="src/styles/main.css">
+    <link rel="stylesheet" href="src/styles/home.css">
     <!-- Police Inter -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -42,7 +43,7 @@
     </main>
     
     <footer>
-        <p>Copyright © - Openclassrooms</p>
+        <p>Bas de page</p>
     </footer>
 
 </body>
--- src/views/templates/Home.php
+++ src/views/templates/Home.php
@@ -1,4 +1,40 @@
-
-<h1>Le super blog</h1>
-<p>super description</p>
-
+<article class="joinHamza">
+    <div class="text">
+        <h2>Rejoignez nos lecteurs passionnés</h2>
+        <p>Donnez une nouvelle vie à vos livres en les échangeant avec d'autres amoureux de la lecture. Nous croyons en la magie du partage de connaissances et d'histoires à travers les livres.</p>
+        <a href="">Découvrir</a>
+    </div>
+    <figure class="picture">
+        <img src="public/img/hamza-nouasria.png" alt="Photo de Hamza Nouasria entourée de livres" />
+        <figcaption>Hamza</figcaption>
+    </figure>
+</article>
+<article class="lastBook">
+    <h2>Les derniers livres ajoutés</h2>
+    <p>card livre</p>
+    <a href="">Voir tous les livres</a>
+</article>
+<article class="howWork">
+    <h2>Comment ça marche ?</h2>
+    <p>Échanger des livres avec TomTroc c’est simple et amusant ! Suivez ces étapes pour commencer :</p>
+    <ul>
+        <li>Inscrivez-vous gratuitement sur notre plateforme.</li>
+        <li>Ajoutez les livres que vous souhaitez échanger à votre profil.</li>
+        <li>Parcourez les livres disponibles chez d'autres membres.</li>
+        <li>Proposez un échange et discutez avec d'autres passionnés de lecture.</li>
+    </ul>
+    <a class="buttonWhiteGreen" href="">Voir tous les livres</a>
+</article>
+<img src="public/img/headband_homepage.png" alt="Photo de Hamza Nouasria entourée de livres" class="headbandHomepage" />
+<article class="valueTomTroc">
+    <h2>Nos valeurs</h2>
+    <p>Chez Tom Troc, nous mettons l'accent sur le partage, la découverte et la communauté. Nos valeurs sont ancrées dans notre passion pour les livres et notre désir de créer des liens entre les lecteurs. Nous croyons en la puissance des histoires pour rassembler les gens et inspirer des conversations enrichissantes.</p>
+    <br/>
+    <p>Notre association a été fondée avec une conviction profonde : chaque livre mérite d'être lu et partagé. </p>
+    <br />
+    <p>Nous sommes passionnés par la création d'une plateforme conviviale qui permet aux lecteurs de se connecter, de partager leurs découvertes littéraires et d'échanger des livres qui attendent patiemment sur les étagères.</p>
+    <div>
+        <p class="teamTomTroc">L’équipe Tom Troc</p>
+        <img src="public/img/heart_green.svg" alt="coeur vert" class="heart" />
+    </div>
+</article>
